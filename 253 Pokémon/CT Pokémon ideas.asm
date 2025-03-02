b1aa6b83ae4f48b798367103075434f5345ad3f5
RaJah239 <ravijagroo239@gmail.com>
2025-03-02 14:48:25 -0400

    Working on Ideas

---
+++ 253 Pokémon/CT Pokémon ideas.asm
@@ -0,0 +1,368 @@
+Pokédex: [max 253]
+So far: 251/253 (2 slots left)
+(130 separate evolution lines so far)
+
+
+
+Chikorita (Grass) -> Lv16
+Bayleef (Grass/Fairy) -> Lv32
+Meganium (Grass/Fairy)
+
+Cyndaquil (Fire) -> Lv14
+Quilava (Fire/Ground) -> Lv36
+Typhlosion (Fire/Ground)
+
+Totodile (Water) -> Lv18
+Croconaw (Water/Dark) -> Lv30
+Feraligatr (Water/Dark)
+
+Budew (Grass/Poison) -> Friendship
+Roselia (Grass/Poison) -> L.Stone
+Roserade (Grass/Poison)
+
+Helioptile (Electric/Normal) -> Sunstone
+Heliolisk (Electric/Normal)
+
+Sunkern (Grass/Fire) -> SunStone
+Sunflora (Grass/Fire)
+
+Pidgey (Normal/Flying) -> Lv18
+Pidgeotto (Normal/Flying) -> Lv36
+Pidgeot (Normal/Flying)
+
+Hoothoot (Flying) -> Friendship @ night
+Noctowl (Flying/Ghost)
+
+Rattata (Normal/Dark) -> Lv20
+Raticate (Normal/Dark)
+
+Buneary (Normal) -> Lv30
+Lopunny (Normal/Fighting)
+
+Teddiursa (Normal) -> Lv30
+Ursaring (Normal)  -> Friendship @ night
+Ursaluna (Ground/Normal)
+
+Shroomish (Grass) -> Lv23
+Breloom (Grass/Fighting)
+
+Gible (Dragon/Ground) -> Lv24
+Gabite (Dragon/Ground) -> Lv48
+Garchomp (Dragon/Ground)
+
+Yanma (Bug) + Floats -> Lv33
+Yanmega (Bug/Dragon) + Floats 
+
+Cutiefly (Bug/Fairy) + Floats -> Lv25
+Ribombee (Bug/Fairy)  + Floats
+
+Zubat(Dark/Flying) -> Lv22
+Goldbat (Dark/Flying) -> Friendship
+Crobat (Dark/Flying)
+
+Spinarak (Bug/Dark) -> Lv28
+Ariados (Bug/Dark)
+
+Poliwag (Water) -> Lv25
+Poliwhirl (Water) -> W.Stone
+Poliwrath (Water/Fighting)
+
+Venipede (Bug/Poison)
+Whirlipede (Bug/Poison) -> Lv22
+Scolipede (Bug/Poison) -> Lv30
+
+Bellsprout (Grass/Poison) -> Lv20
+Weepinbell (Grass/Poison) -> L.Stone
+Victreebel (Grass/Poison)
+
+Mareep (Electric) -> Lv15
+Flaaffy (Electric) -> Lv30
+Ampharos (Electric/Dragon)
+
+Rhyhorn (Ground/Rock) -> Trade
+Rhydon (Ground/Rock) (fast ground/rock mon, like 82 speed)
+
+Magnemite (Electric/Steel) + Floats -> Lv30
+Magneton (Electric/Steel) + Floats -> T.Stone
+Magnezone (Electric/Steel) + Floats
+
+Remoraid (Water) -> Lv25
+Octillery (Water/Fire)
+
+Chinchou (Water/Electric) -> Lv27
+Lanturn (Water/Electric)
+
+Feebas (Water) -> Friendship
+Milotic (Water)
+
+Magikarp (Water) -> D.Stone
+Gyarados (Water/Dragon)
+
+Horsea (Water) -> 26
+Seadra (Water) -> D.Stone
+Kingdra (Water/Dragon)
+
+Natu (Psychic/Flying) -> Lv25
+Xatu (Psychic/Flying)
+
+Phantump (Ghost/Grass) -> Lv25
+Trevenant (Ghost/Grass)
+
+Duskull (Ghost) -> Lv25
+Dusclops (Ghost) -> Trade
+Dusknoir + Floats
+
+Gastly (Ghost/Poison) -> Lv25
+Haunter (Ghost) -> Trade
+Gengar (Ghost/Psychic)
+
+Bulbasaur (Grass/Rock) -> Lv16
+Ivysaur (Grass/Rock) -> L.Stone
+Venusaur (Grass/Rock)
+
+Vulpix (Fire) -> F.Stone
+Ninetales (Fire/Ghost) + Floats
+
+Houndour (Fire/Dark)
+Houndoom (Fire/Dark) -> 24
+
+Togepi (Normal/Fairy) -> Friendship
+Togetic (Normal/Fairy) + Floats -> Trade
+Togekiss (Normal/Fairy) + Floats
+
+Joltik (Bug/Electric) -> Lv36
+Galvantula (Bug/Electric)
+
+Doduo (Flying/Fighting) -> 28
+Dodrio (Flying/Fighting)
+
+Skorupi (Poison/Bug) -> Lv40
+Drapion (Poison/Dark)
+
+Wooper (Water/Ground) -> 20
+Quagsire (Water/Ground)
+
+Marill (Water/Fairy) -> 22
+Azurill (Water/Fairy)
+
+Cubone (Ground) -> Friendship @ night
+Marowak (Ground/Ghost)
+
+Diglett (Ground) -> Lv26
+Dugtrio (Ground)
+
+Drilbur (Ground) -> Lv31
+Excadrill (Ground/Steel)
+
+Onix (Rock/Dragon) -> M.Coat
+; (Give similar stats to Tyrantrum but 500 BST, more defense),
+; 0%? 1%? encounter rate in Union Cave.
+; Something else gets traded in Violet City.
+Steelix (Steel/Dragon)
+
+Ponyta (Fire/Fairy) -> Lv40
+Rapidash (Fire/Fairy)
+
+Slowpoke (Water/Psychic) -> Lv37
+Slowking (Psychic/Poison)
+
+Venonat (Bug/Posion) -> Friendship @ day
+Psymoth (Bug/Psychic) + Floats
+
+Paras (Bug/Grass) -> Lv28?
+Parasect (Bug/Ghost)
+
+Exeggcute (Grass) -> D.Stone
+A.Exeggutor (Grass/Dragon)
+
+Gligar (Ground/Flying) -> Friendship
+Gliscor (Ground/Flying)
+
+Psyduck (Water/Psychic) -> Lv33
+Golduck (Water/Psychic)
+
+Pikachu (Electric) + Floats -> Thundestone
+Raichu (Electric/Psychic)
+
+Mankey (Fighting/Dark) -> Friendship @ night
+Primeape (Fighting/Dark)
+
+Nidoran♀ (Poison) -> Lv16
+Nidorino (Poison) -> D.Stone
+Nidoking (Poison/Dragon)
+
+Nidoran♂ (Poison) -> Lv16
+Nidorina (Poison) -> Moonstone
+Nidoqueen (Poison/Fairy) - Similar stats to Fezandipiti
+
+Growlithe (Fire/Normal) -> F.Stone
+Arcanine (Fire/Normal)
+
+Meowth (Dark/Steel) -> Lv28
+Persian (Dark/Steel)
+
+Snubbull (Fighting/Fairy) -> Lv28
+Granbull (Fighting/Fairy)
+
+Ditto (Normal)
+
+Voltorb (Electric/Grass) -> L.Stone
+Electrode (Electric/Grass)
+
+Clefairy (Fairy/Steel) -> Moonstone
+Clefable (Fairy/Steel)
+
+Drowzee (Psychic/Dark) -> Lv26
+Hypno (Psychic/Dark)
+
+Charmander (Fire/Dragon) -> Lv16
+Charmeleon (Fire/Dragon) -> F.Stone
+Charizard (Fire/Dragon) + Floats
+
+Tauros (Normal)
+Mr. Mime (Psychic/Fairy)
+Jynx (Ice/Psychic)
+Electabuzz (Electric/Fighting)
+Magmar (Fire/Fighting)
+
+Pinsir (Bug/Ground)
+Scyther (Bug/Flying) -> M.Coat
+Scizor (Bug/Steel) + Floats
+Heracross (Bug/Fighting)
+Shuckle (Rock/Bug)
+
+Koffing (Poison/Fire) + Floats -> lv30
+Weezing (Poison/Fire) + Floats
+
+Grimer (Poison) -> lv30
+Muk  (Poison)
+
+Chansey (Normal) -> Friendship @ day
+Blissey (Normal)
+
+Riolu (Fighting) -> Friendship @ day
+Lucario (Fighting/Steel)
+
+Bronzor (Steel/Psychic) + Floats -> Lv33
+Bronzong (Steel/Psychic) + Floats 
+
+Skarmory (Steel/Flying)
+
+Sudowoodo (Rock/Fighting)
+
+Ferroseed (Grass/Steel) -> Lv40
+Ferrothorn (Grass/Steel)
+
+Miltank (Normal/Fairy)
+
+Smeargle (Normal)
+
+Eevee (Normal) -> Friendship @ Day for Sylveon & Friendship @ Night for Umbreon
+Umbreon (Dark)
+Sylveon (Fairy)
+
+Squirtle (Water/Steel) -> Lv16
+Wartortle (Water/Steel) -> W.Stone
+Blastoise (Water/Steel)
+
+Tangela (Grass/Water) -> Lv30
+Tangrowth (Grass/Water)
+
+Krabby (Water/Bug)  -> Lv28
+Kingler (Water/Bug) 
+
+Staryu (Water/Psychic) -> W.Stone
+Starmie (Water/Psychic)
+
+Mantine (Water)
+
+Tentacool (Water/Poison) -> Lv30
+Tentacruel (Water/Poison)
+
+Corsola (Rock/Fairy)
+
+Seel (Water) -> Lv34
+Dewgong (Water/Ice)
+
+Stantler (Normal) -> Friendship
+Wyrdeer (Normal/Psychic)
+
+Shiftry (Grass/Dark)
+
+Phanpy (Ground) -> Lv31
+Donphan (Ground/FIghting)
+
+Alolan.Sandshrew (Ice/Steel) -> Friendship
+Alolan.Sandslash (Ice/Steel)
+
+Snover (Grass/Ice)
+Abomasnow (Grass/Ice)
+
+Delibird (Ice/Fairy)
+
+Snorunt (Ice) -> Friendship @ day to Glalie or Friendship @ night to Froslass
+Glalie (Ice)
+Froslass (Ice/Ghost)
+
+Swinub (Ice/Ground) -> Lv33
+Piloswine (Ice/Ground)
+
+Sneasel (Dark/Ice) -> Weavile Friendship @ night, Sneasler Friendship @ day
+Weavile (Dark/Ice)
+Sneasler (Fighting/Poison)
+
+Misdreavus (Ghost) -> Moonstone
+Miss Magius (Ghost/Fairy)
+
+Lapras (Dragon/Ice)
+
+Porygon (Electric/Normal) -> Friendship
+Porygon2 (Electric/Normal) -> Trade
+Porygon-Z (Electric/Normal) + Floats
+
+Omanyte (Rock/Water) -> Lv36
+Omastar (Rock/Water) (Barbaracle like stats)
+Kabuto (Rock/Bug) -> Lv36
+Kabutops (Rock/Bug)
+Aerodactyl (Rock/Flying)
+
+Solrock (Rock/Fire) + Floats 
+Lunatone (Rock/Psychic) + Floats 
+Spiritomb (Ghost/Dark) + Floats
+
+Snorlax (Normal)
+
+Larvitar (Rock/Dark) -> Lv30
+Pupitar (Rock/Dark) -> Lv55
+Tyranitar (Rock/Dark)
+
+Volcarona (Bug/Fire)
+Hydreigon (Dark/Dragon) + Floats
+Dragapult (Dragon/Ghost)  + Floats
+
+Dratini (Dragon) -> Lv30
+Dragonair (Dragon) -> Lv55
+Dragonite (Dragon) + Floats, similar stat spread to Haxorus but 600 BST
+
+Regirock (Rock)
+Regice (Ice)
+Registeel (Steel)
+Regigigas (Normal)
+
+Articuno (Ice/Flying)
+Zapdos (Electric/Flying)
+Moltres (Fire/Flying)
+Lugia (Water/Flying)
+
+Raikou (Electric)
+Entei (Fire)
+Suicune (Water)
+Ho-Oh (Fire/Flying)
+
+Celebi (Psychic/Grass) + Floats
+
+Mewtwo (Psychic/Fighting)
+
+Unown (Psychic) + Floats 
+Mew (Psychic/Normal) + Floats 
+MISSINGNO/PHANCERO (???) + Floats
