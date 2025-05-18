b103b539e92914ae451efc5b9ad0dbf452a1ebef
RaJah239 <ravijagroo239@gmail.com>
2025-05-18 16:24:00 -0400

    Added files via upload + Readme Update

---
+++ files/Town Map Ideas.asm
@@ -0,0 +1,45 @@
+
+Minor Island:
+
+Pokemon League
+Get Waterfall scaling item here
+Battle Tower
+6 vs 6 battles at max level
+champion's house?
+Gym/Leaders/Main Named Characters special house?
+reverse battle house
+
+Badges: 7
+
+1st Half
+1. Piarco - Zephyr Badge (FLY HM), get HEDGER to cut through forest
+2. Botanic Garden -> Bug Catching Contest -> Leader Erika (Grass/Rainbow Badge) POS
+7. Small Island West of Diego Martin -> Water Badge
+8. Laentille -> Poison Badge
+
+
+Second Half
+3. Waterloo Cremation Site -> FogBadge
+4. Mud/ Volcano -> Fire Badge (Strength/POwer glove)
+5. Penal - Debe -> Steel Type gym (WHIRLPOOL Item)
+6. Power Plant -> Do a quest to earn the right to fight (Thunder Badge) (Point Fortin) (PADDLE BOAT)
+
+Main Land:
+
+Mountains -> Ice mountains, snowy, north of Main Land
+
+Port Town with Light House, here's how to go to Minor Island as the currents around are too strong, then can fly
+
+Forest (like Ilex or closer to Viridian Forest)
+
+
+
+
+
+
+
+
+
+
+
+
