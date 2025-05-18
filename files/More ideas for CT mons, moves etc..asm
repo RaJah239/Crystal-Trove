a0fe385cbc7fe090045cbbabe20e477dcb210e67
RaJah239 <ravijagroo239@gmail.com>
2025-05-18 16:24:00 -0400

    Added files via upload + Readme Update

---
+++ files/More ideas for CT mons, moves etc..asm
@@ -0,0 +1,136 @@
+Caterpie line -> Ribombee (floats)
+Replace Weedle line -> Scolipede line
+Sandshrew line -> Drillbur/Excadrill line
+Ledian line -> Joltik line
+Foretrees line -> ferrothorn line
+Sentret line - replaced by cinccino line? or just take their stats + learnset? 
+Drapion line replaces Ekans & Arbok line
+
+Type changes:
+Noctowl - Redistribute Drifblim stats for it, more bulk, less attack
+Rhydon - 505 BST, 100 Base Speed
+Trevenant - bulkier, more defense, round stats to 500 BST at lake of rage, have people say that the route there can be scary at night in Mahogany town
+Muk - Bulkier, buffed stats, pure poison still
+Nidoking -> Poison/Dragon (?)
+Nidoqueen -> Poison/Steel (?)
+Zubat/Crobat -> Flying/Dark
+Parascet - Bug/Ghost, recolour it to be more grey and red
+Meowth line -> Normal/Steel
+Primeape -> Fighting/Dark, moves pursuit, feint attack, Snarl, theif(?), throat chop, Annihilape’s stats or Mienshao's
+Kingler line - Water/Rock similar stats to Klawf
+Growlithe line -> Fire/Normal
+Bellsprout line -> Grass/Electric
+Doduo line -> Flying/Fighting (learns play rough too?) dodrio similar stats to Hawlucha or Flamigo
+Drowzee line -> Psychic/Dark
+Voltorb line -> Electric/Normal
+Exeggutor -> Alolan Exeggutor instead (Grass/Dragon)
+Tangela -> Grass/Water
+Starmie -> Water/Fairy
+Pinsir -> Bug/Ground
+Kabuto -> Rock/Bug
+Ariados -> Bug/Dark
+Donphan -> Ground/Fighting - stats like Great Tusk and moveset too
+Add Yanmega - Bug/Dragon + Floating
+Snubull line -> Fighting/Fairy + learns bites moves
+Sneasel to evolve into friendship night Weavile and friendship day Sneasler on different circumstances
+Ursaring -> Blood moon ursuluna ground/normal stats
+Miltank -> Normal/Fairy
+Lapras -> Dragon/Ice
+Togetic - Fairy/Normal  Float
+Xatu - Sigilyph stats
+Nintetales -> Chandelure stats + Ghost/Fire + Float
+Replace Omastar with Cradily and do over Unown puzzle with Lelipep instead 
+Skarmory - Corviknight stats or similar 
+Entei- similar to Gouging Fire stats
+Ninetales gets A.Ninetales stats
+Sudowoodo -> Rock/Fighting, Terrakion like stats
+Snover -> Violet, trade Bellsprout for it or breed. + 1% in Ice path
+
+
+Add Move:
+Snarl -> Use Snore animation
+Throat Chop (Dark) -> Inverted Cross Chop animation, crit move, 95 Acc, 80 damage (TM?)
+Dragon Rush? Same animation as double edge?
+
+Pokemon to add:
+Solrock - Rock/Fire 80 SpAtk 100 Defense? Slow floats
+Lunatone - Rock/Psychic 80 Atk, 100 Defenses? Slow floats
+lopunny line to have normal/fighting? and early game mon
+
+remove for now:
+Sewaddle (Bug/Grass) -> Lv20
+Swadloon (Bug/Grass) -> Friendship @ day
+Leavanny (Bug/Grass)
+
+Whimsicott line?
+
+Have the Goldenrod Stone Mart no sell to the player until xx number of badges.
+
+Type Changes:
+Ice to resist: Ice, Water, Electric, Flying
+
+Solrock and Lunatone sprites from Pokewilds
+
+Moltres - Talonflame speed
+
+Gyarados gets gen1 stats. Kingdra gets swapped Gen1 Atk and Def for SpAtk & SpDef but rest is same stats?
+
+Make Maganieum a Grass/Dragon. Evolve via sin stone? Replace Execute line with that Grass/Fairy mon wminiscoot?
+
+Staraptor-like stats to pidgeot?
+
+Omastar gets Garganacl like stats
+
+Kleavor.Durant like stats to scizor?
+
+Consider Eelektross line?
+
+Quagsire gets Seismitoad-like stats
+
+Sawk-like stats for Poliwrath
+
+Piloswine - Mamoswine like stats that max at 505
+
+Urshifu like stats for Primeape?
+
+Sigilyph-like stats for Xatu
+
+Vanilluxe stats for Abamasnow or with a mix of Cryogonal stats?
+
+Chandelure stats for Ninetales?
+
+Klinklang like stats for Onix?
+
+Corsola stats like Carbink
+
+Tangel gets Ludicolo like stats
+
+Slowking (Psychic/Poison) - check out Grate Oracle's sprite
+
+==================================================================
+
+Regigigas' Stats:
+HP: 160
+Attack: 110
+Defense: 200
+Special Attack: 110
+Special Defense: 50
+Speed: 50
+BST: 680
+
+==================================================================
+
+Add
+===
+
+Moves:
+Signal Beam - Clefairy learns it lol
+Rough Play - 80 damage, 100% acc, 10% attack decrease opponent 
+Foul Play - 80 damage, dark counter part to Play Rough, 100% acc, 10% attack increase (inverted Play rough animation?)
+
+Removed for now:
+================
+
+Sandile (Ground/Dark) -> 29
+Krokorok (Ground/Dark) -> 40
+Krookodile (Ground/Dark)
