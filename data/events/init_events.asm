922cd5f8be027a3a0dde66350d2ba6a43e77b833
RaJah239 <ravijagroo239@gmail.com>
2025-05-26 08:48:31 -0400

    Caught level, time and location + Effort Values + Unlock Hidden Power type (via Unown quest) on fourth stat page

--- data/events/init_events.asm
+++ data/events/init_events.asm
@@ -125,6 +125,7 @@ InitialEvents:
 	dw EVENT_INITIALIZED_EVENTS
 if DEF(_DEBUG)
 	dw EVENT_GAVE_MYSTERY_EGG_TO_ELM
+	dw EVENT_CAUGHT_ALL_UNOWN
 endc
 ; Place new events that initialises on game start
 	dw -1 ; end
