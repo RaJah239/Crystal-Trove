8a6a41814ddf14622c8abb3043fa97d831901a64
RaJah239 <ravijagroo239@gmail.com>
2025-05-28 15:02:58 -0400

    Obedience set at Level Caps (need to edit gym text)

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -677,23 +677,43 @@ BattleCommand_CheckObedience:
 	ld a, MAX_LEVEL + 1
 	jr nz, .getlevel
 
+	; mineralbadge
+	bit MINERALBADGE, [hl]
+	ld a, 42
+	jr nz, .getlevel
+
 	; stormbadge
 	bit STORMBADGE, [hl]
-	ld a, 70
+	ld a, 37
+	jr nz, .getlevel
+
+	; glacierbadge
+	bit GLACIERBADGE, [hl]
+	ld a, 36
 	jr nz, .getlevel
 
 	; fogbadge
 	bit FOGBADGE, [hl]
-	ld a, 50
+	ld a, 31
+	jr nz, .getlevel
+
+	; plainbadge
+	bit PLAINBADGE, [hl]
+	ld a, 26
 	jr nz, .getlevel
 
 	; hivebadge
 	bit HIVEBADGE, [hl]
-	ld a, 30
+	ld a, 21
+	jr nz, .getlevel
+
+	; zephyrbadge
+	bit ZEPHYRBADGE, [hl]
+	ld a, 16
 	jr nz, .getlevel
 
 	; no badges
-	ld a, 10
+	ld a, 9
 
 .getlevel
 ; c = obedience level
