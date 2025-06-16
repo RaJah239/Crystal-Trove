dba618a9feb14d31a16afe69978de666aeb50f7e
RaJah239 <ravijagroo239@gmail.com>
2025-06-16 14:27:06 -0400

    Removed Sweet Scent for Mon Menu

--- engine/pokemon/mon_menu.asm
+++ engine/pokemon/mon_menu.asm
@@ -146,7 +146,6 @@ PokemonActionSubmenu:
 	dbw MONMENUITEM_MILKDRINK,  MonMenu_Softboiled_MilkDrink
 	dbw MONMENUITEM_HEADBUTT,   MonMenu_Headbutt
 	dbw MONMENUITEM_ROCKSMASH,  MonMenu_RockSmash
-	dbw MONMENUITEM_SWEETSCENT, MonMenu_SweetScent
 	dbw MONMENUITEM_STATS,      OpenPartyStats
 	dbw MONMENUITEM_SWITCH,     SwitchPartyMons
 	dbw MONMENUITEM_ITEM,       GiveTakePartyMonItem
@@ -869,12 +868,6 @@ MonMenu_RockSmash:
 
 .Fail:
 	ld a, $3
-	ret
-
-MonMenu_SweetScent:
-	farcall SweetScentFromMenu
-	ld b, $4
-	ld a, $2
 	ret
 
 ChooseMoveToDelete:
