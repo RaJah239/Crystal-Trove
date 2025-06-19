01dfc84ab6541eb5b7139259e67b275e4c5dd298
RaJah239 <ravijagroo239@gmail.com>
2025-06-19 07:05:15 -0400

    New trees added to Route 30

--- constants/event_flags.asm
+++ constants/event_flags.asm
@@ -292,7 +292,11 @@
 	const EVENT_ROUTE_1_APRICORN
 	const EVENT_ROUTE_1_BERRY1
 	const EVENT_ROUTE_1_BERRY2
-; Unused: next 317 events
+	const EVENT_ROUTE_2_BERRY1
+	const EVENT_ROUTE_2_BERRY2
+	const EVENT_ROUTE_2_APRICORN1
+	const EVENT_ROUTE_2_APRICORN2
+; Unused: next 313 events
 
 	const_next 600
 ; Kurt Apricorn events
