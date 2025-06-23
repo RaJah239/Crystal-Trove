acb28d3951765dac9c3c350682ad42af3a3cff72
RaJah239 <ravijagroo239@gmail.com>
2025-06-23 11:20:53 -0400

    New trees added to Route 44

--- constants/event_flags.asm
+++ constants/event_flags.asm
@@ -328,8 +328,10 @@
 	const EVENT_ROUTE_42_APRICORN_3    ; YLW_APRICORN
 	const EVENT_ROUTE_43_BERRY_1       ; BITTER_BERRY
 	const EVENT_ROUTE_43_APRICORN_1    ; PNK_APRICORN
-
-; Unused: next 281 events
+	const EVENT_ROUTE_44_BERRY_1       ; BURNT_BERRY
+	const EVENT_ROUTE_44_BERRY_2       ; MIRACLEBERRY
+	const EVENT_ROUTE_44_APRICORN_1    ; WHT_APRICORN
+; Unused: next 278 events
 
 	const_next 600
 ; Kurt Apricorn events
