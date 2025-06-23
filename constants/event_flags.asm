7c1257a70934acabae69ec3388d17e23c2e33be4
RaJah239 <ravijagroo239@gmail.com>
2025-06-23 11:07:49 -0400

    New trees added to Route 43

--- constants/event_flags.asm
+++ constants/event_flags.asm
@@ -326,8 +326,10 @@
 	const EVENT_ROUTE_42_APRICORN_1    ; PNK_APRICORN
 	const EVENT_ROUTE_42_APRICORN_2    ; GRN_APRICORN
 	const EVENT_ROUTE_42_APRICORN_3    ; YLW_APRICORN
+	const EVENT_ROUTE_43_BERRY_1       ; BITTER_BERRY
+	const EVENT_ROUTE_43_APRICORN_1    ; PNK_APRICORN
 
-; Unused: next 283 events
+; Unused: next 281 events
 
 	const_next 600
 ; Kurt Apricorn events
