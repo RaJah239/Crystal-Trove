8c2f06c4d558b497a90d89faa4cee0a85491b5f9
RaJah239 <ravijagroo239@gmail.com>
2025-06-30 20:04:32 -0400

    Critical Hits do 1.5x damage, instead of 2x

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -3238,21 +3238,38 @@ DEF DAMAGE_CAP EQU MAX_DAMAGE - MIN_DAMAGE
 	and a
 	ret z
 
-; x2
-	ldh a, [hQuotient + 3]
-	add a
-	ldh [hQuotient + 3], a
+; x1.5 (multiply by 15 and divide by 10)
+	xor a
+	ldh [hMultiplicand + 0], a
 
 	ldh a, [hQuotient + 2]
-	rl a
-	ldh [hQuotient + 2], a
+	ldh [hMultiplicand + 1], a
+
+	ldh a, [hQuotient + 3]
+	ldh [hMultiplicand + 2], a
+
+	ld a, 15
+	ldh [hMultiplier], a
+
+	call Multiply
+
+	ld a, 10
+	ldh [hDivisor], a
+	ld b, 4
+	call Divide
+
+	ldh a, [hQuotient + 3]
+	ldh [hProduct + 3], a
+
+	ldh a, [hQuotient + 2]
+	ldh [hProduct + 2], a
 
 ; Cap at $ffff.
 	ret nc
 
 	ld a, $ff
-	ldh [hQuotient + 2], a
-	ldh [hQuotient + 3], a
+	ldh [hProduct + 2], a
+	ldh [hProduct + 3], a
 
 	ret
 
