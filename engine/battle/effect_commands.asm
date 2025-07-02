e4ae785938a6b60a75216a46aa880ec98f86d1fd
RaJah239 <ravijagroo239@gmail.com>
2025-07-02 12:30:45 -0400

    Implemented Typeless Battles (all types are neutral to each other) Credit: https://github.com/pret/pokecrystal/wiki/Inverse-Battles#addendum-neutral-battles

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -1275,12 +1275,18 @@ BattleCommand_Stab:
 	and TYPE_MASK
 	ld b, a
 	ld a, [wBattleType]
+	cp BATTLETYPE_TYPELESS
+	jr z, .typeless
 	cp BATTLETYPE_INVERSE
 	jr z, .inverse
 	ld hl, TypeMatchups
 	jr .TypesLoop
 .inverse
  	ld hl, InverseTypeMatchups
+ 	jr .TypesLoop
+.typeless
+	ld hl, NoTypeMatchups
+
 .TypesLoop:
 	call GetNextTypeMatchupsByte
 	inc hl
@@ -1406,12 +1412,18 @@ CheckTypeMatchup:
 	ld a, EFFECTIVE
 	ld [wTypeMatchup], a
 	ld a, [wBattleType]
+	cp BATTLETYPE_TYPELESS
+	jr z, .typeless
 	cp BATTLETYPE_INVERSE
 	jr z, .inverse
 	ld hl, TypeMatchups
 	jr .TypesLoop
 .inverse
 	ld hl, InverseTypeMatchups
+	jr .TypesLoop
+.typeless
+	ld hl, NoTypeMatchups
+
 .TypesLoop:
 	call GetNextTypeMatchupsByte
 	inc hl
