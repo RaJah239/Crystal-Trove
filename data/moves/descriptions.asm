3b1028135de64751d2de4b5a40fd9ba8c60ea22b
RaJah239 <ravijagroo239@gmail.com>
2025-07-15 13:58:56 -0400

    Earth Power added

--- data/moves/descriptions.asm
+++ data/moves/descriptions.asm
@@ -140,7 +140,7 @@ MoveDescriptions::
 	dw GlareDescription
 	dw DreamEaterDescription
 	dw PoisonGasDescription
-	dw BarrageDescription
+	dw EarthPowerDescription
 	dw LeechLifeDescription
 	dw LovelyKissDescription
 	dw SkyAttackDescription
@@ -596,6 +596,9 @@ ConfusionDescription:
 	db   "An attack that may"
 	line "cause confusion.@"
 
+EarthPowerDescription:
+FocusBlastDescription:
+ShadowBallDescription:
 FlashCannonDescription:
 BugBuzzDescription:
 PsychicMDescription:
@@ -758,10 +761,6 @@ DreamEaterDescription:
 PoisonGasDescription:
 	db   "A move that may"
 	line "poison the foe.@"
-
-BarrageDescription:
-	db   "Throws orbs to hit"
-	line "2-5 times.@"
 
 LovelyKissDescription:
 	db   "May cause the foe"
@@ -1148,11 +1147,6 @@ ExtremespeedDescription:
 AncientpowerDescription:
 	db   "An attack that may"
 	line "raise all stats.@"
-
-FocusBlastDescription:
-ShadowBallDescription:
-	db   "An attack that may"
-	line "lower Spcl.Def.@"
 
 StealthRockDescription:
 	db   "Hurts foes when"
--- data/pokemon/evos_attacks.asm
+++ data/pokemon/evos_attacks.asm
@@ -1226,7 +1226,6 @@ ElectrodeEvosAttacks:
 ExeggcuteEvosAttacks:
 	db EVOLVE_ITEM, LEAF_STONE, EXEGGUTOR
 	db 0 ; no more evolutions
-	db 1, BARRAGE
 	db 1, HYPNOSIS
 	db 7, REFLECT
 	db 13, LEECH_SEED
@@ -1239,7 +1238,6 @@ ExeggcuteEvosAttacks:
 
 ExeggutorEvosAttacks:
 	db 0 ; no more evolutions
-	db 1, BARRAGE
 	db 1, HYPNOSIS
 	db 1, CONFUSION
 	db 19, STOMP
