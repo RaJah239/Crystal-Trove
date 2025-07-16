a10eb8a55846c897681d441d81755d41e04e4521
RaJah239 <ravijagroo239@gmail.com>
2025-07-16 14:48:45 -0400

    Removed some usused AI code

--- engine/battle/ai/scoring.asm
+++ engine/battle/ai/scoring.asm
@@ -477,219 +477,129 @@ AI_Smart_Switch:
     ld [wEnemyIsSwitching], a
 	ret
 
+; DevNote - this is removed because this was stupid and will not be used
 AI_Setup:
-; Use stat-modifying moves on turn 1.
+	ret
 
-; 50% chance to greatly encourage stat-up moves during the first turn of enemy's Pokemon.
-; 50% chance to greatly encourage stat-down moves during the first turn of player's Pokemon.
-; 100% chance to greatly encourage stat-up moves if the player is flying or underground, and the enemy is faster.
-; 100% chance to greatly discourage stat-down moves if the player has Mist or a Substitute up.
-; Almost 90% chance to greatly discourage stat-modifying moves otherwise.
-
-	ld hl, wEnemyAIMoveScores - 1
-	ld de, wEnemyMonMoves
-	ld b, NUM_MOVES + 1
-.checkmove
-	dec b
-	ret z
-
-	inc hl
-	ld a, [de]
-	and a
-	ret z
-
-	inc de
-	call AIGetEnemyMove
-
-	ld a, [wEnemyMoveStruct + MOVE_EFFECT]
-
-	cp EFFECT_ATTACK_UP
-	jr c, .checkmove
-	cp EFFECT_EVASION_UP + 1
-	jr c, .statup
-
-;	cp EFFECT_ATTACK_DOWN - 1
-	jr z, .checkmove
-	cp EFFECT_EVASION_DOWN + 1
-	jr c, .statdown
-
-	cp EFFECT_ATTACK_UP_2
-	jr c, .checkmove
-	cp EFFECT_EVASION_UP_2 + 1
-	jr c, .statup
-
-;	cp EFFECT_ATTACK_DOWN_2 - 1
-	jr z, .checkmove
-	cp EFFECT_EVASION_DOWN_2 + 1
-	jr c, .statdown
-
-	jr .checkmove
-
-.statup
-	ld a, [wPlayerSubStatus3]
-	and 1 << SUBSTATUS_FLYING | 1 << SUBSTATUS_UNDERGROUND
-	jr z, .statup_continue
-
-	call AICompareSpeed
-	jr c, .do_encourage
-
-.statup_continue
-	ld a, [wEnemyTurnsTaken]
-	and a
-	jr nz, .discourage
-
-	jr .encourage
-
-.statdown
-	ld a, [wPlayerSubStatus4]
-	bit SUBSTATUS_MIST, a
-	jr nz, .do_discourage
-
-	ld a, [wPlayerSubStatus4]
-	bit SUBSTATUS_SUBSTITUTE, a
-	jr nz, .do_discourage
-
-	ld a, [wPlayerTurnsTaken]
-	and a
-	jr nz, .discourage
-
-.encourage
-	call AI_50_50
-	jr c, .checkmove
-
-.do_encourage
-	dec [hl]
-	dec [hl]
-	jr .checkmove
-
-.discourage
-	call Random
-	cp 12 percent
-	jr c, .checkmove
-
-.do_discourage
-	inc [hl]
-	inc [hl]
-	jr .checkmove
-
-
+; DevNote - this is commented out because this is made redundant by AI_Aggressive
 AI_Types:
 ; Dismiss any move that the player is immune to.
 ; Encourage super-effective moves.
 ; Discourage not very effective moves unless
 ; all damaging moves are of the same type.
-
-	ld hl, wEnemyAIMoveScores - 1
-	ld de, wEnemyMonMoves
-	ld b, NUM_MOVES + 1
-.checkmove
-	dec b
-	ret z
-
-	inc hl
-	ld a, [de]
-	and a
-	ret z
-
-	inc de
-	call AIGetEnemyMove
-
-	push hl
-	push bc
-	push de
-	ld a, 1
-	ldh [hBattleTurn], a
-	callfar BattleCheckTypeMatchup
-	pop de
-	pop bc
-	pop hl
-
-	ld a, [wTypeMatchup]
-	and a
-	jr z, .immune
-	cp EFFECTIVE
-	jr z, .checkmove
-	jr c, .noteffective
-
+;
+;	ld hl, wEnemyAIMoveScores - 1
+;	ld de, wEnemyMonMoves
+;	ld b, NUM_MOVES + 1
+;.checkmove
+;	dec b
+;	ret z
+;
+;	inc hl
+;	ld a, [de]
+;	and a
+;	ret z
+;
+;	inc de
+;	call AIGetEnemyMove
+;
+;	push hl
+;	push bc
+;	push de
+;	ld a, 1
+;	ldh [hBattleTurn], a
+;	callfar BattleCheckTypeMatchup
+;	pop de
+;	pop bc
+;	pop hl
+;
+;	ld a, [wTypeMatchup]
+;	and a
+;	jr z, .immune
+;	cp EFFECTIVE
+;	jr z, .checkmove
+;	jr c, .noteffective
+;
 ; effective
-	ld a, [wEnemyMoveStruct + MOVE_POWER]
-	and a
-	jr z, .checkmove
-	dec [hl]
-	jr .checkmove
-
-.noteffective
+;	ld a, [wEnemyMoveStruct + MOVE_POWER]
+;	and a
+;	jr z, .checkmove
+;	dec [hl]
+;	jr .checkmove
+;
+;.noteffective
 ; Discourage this move if there are any moves
 ; that do damage of a different type.
-	push hl
-	push de
-	push bc
-	ld a, [wEnemyMoveStruct + MOVE_TYPE]
-	and TYPE_MASK
-	ld d, a
-	ld hl, wEnemyMonMoves
-	ld b, NUM_MOVES + 1
-	ld c, 0
-.checkmove2
-	dec b
-	jr z, .movesdone
+;	push hl
+;	push de
+;	push bc
+;	ld a, [wEnemyMoveStruct + MOVE_TYPE]
+;	and TYPE_MASK
+;	ld d, a
+;	ld hl, wEnemyMonMoves
+;	ld b, NUM_MOVES + 1
+;	ld c, 0
+;.checkmove2
+;	dec b
+;	jr z, .movesdone
+;
+;	ld a, [hli]
+;	and a
+;	jr z, .movesdone
+;
+;	call AIGetEnemyMove
+;	ld a, [wEnemyMoveStruct + MOVE_TYPE]
+;	and TYPE_MASK
+;	cp d
+;	jr z, .checkmove2
+;	ld a, [wEnemyMoveStruct + MOVE_POWER]
+;	and a
+;	jr nz, .damaging
+;	jr .checkmove2
+;
+;.damaging
+;	ld c, a
+;.movesdone
+;	ld a, c
+;	pop bc
+;	pop de
+;	pop hl
+;	and a
+;	jr z, .checkmove
+;	inc [hl]
+;	jr .checkmove
+;
+;.immune
+;	call AIDiscourageMove
+;	jr .checkmove
 
-	ld a, [hli]
-	and a
-	jr z, .movesdone
-
-	call AIGetEnemyMove
-	ld a, [wEnemyMoveStruct + MOVE_TYPE]
-	and TYPE_MASK
-	cp d
-	jr z, .checkmove2
-	ld a, [wEnemyMoveStruct + MOVE_POWER]
-	and a
-	jr nz, .damaging
-	jr .checkmove2
-
-.damaging
-	ld c, a
-.movesdone
-	ld a, c
-	pop bc
-	pop de
-	pop hl
-	and a
-	jr z, .checkmove
-	inc [hl]
-	jr .checkmove
-
-.immune
-	call AIDiscourageMove
-	jr .checkmove
-
-
+; DevNote - this is commented out because it is stupid and wont be used
 AI_Offensive:
+	ret
 ; Greatly discourage non-damaging moves.
-
-	ld hl, wEnemyAIMoveScores - 1
-	ld de, wEnemyMonMoves
-	ld b, NUM_MOVES + 1
-.checkmove
-	dec b
-	ret z
-
-	inc hl
-	ld a, [de]
-	and a
-	ret z
-
-	inc de
-	call AIGetEnemyMove
-
-	ld a, [wEnemyMoveStruct + MOVE_POWER]
-	and a
-	jr nz, .checkmove
-
-	inc [hl]
-	inc [hl]
-	jr .checkmove
+;
+;	ld hl, wEnemyAIMoveScores - 1
+;	ld de, wEnemyMonMoves
+;	ld b, NUM_MOVES + 1
+;.checkmove
+;	dec b
+;	ret z
+;
+;	inc hl
+;	ld a, [de]
+;	and a
+;	ret z
+;
+;	inc de
+;	call AIGetEnemyMove
+;
+;	ld a, [wEnemyMoveStruct + MOVE_POWER]
+;	and a
+;	jr nz, .checkmove
+;
+;	inc [hl]
+;	inc [hl]
+;	jr .checkmove
 
 
 AI_Smart:
