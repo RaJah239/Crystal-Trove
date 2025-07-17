c52ef603a1a9711d266f30484ca62eb2cfd7e544
RaJah239 <ravijagroo239@gmail.com>
2025-07-17 08:51:30 -0400

    Rock Slide reverted to vanilla game's damage output

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -170,7 +170,7 @@ Moves:
 	move FURY_STRIKES, EFFECT_MULTI_HIT,          18, NORMAL,        PHYSICAL,  80, 15,   0
 	move HIDDEN_FORCE, EFFECT_HIDDEN_POWER,       70, CURSE_TYPE,    PHYSICAL, 100, 24,   0
 	move REST,         EFFECT_HEAL,                0, PSYCHIC_TYPE,  STATUS,   100, 10,   0
-	move ROCK_SLIDE,   EFFECT_FLINCH_HIT,         80, ROCK,          PHYSICAL, 100, 24,  15
+	move ROCK_SLIDE,   EFFECT_FLINCH_HIT,         75, ROCK,          PHYSICAL,  90, 24,  30
 	move HYPER_FANG,   EFFECT_FLINCH_HIT,         80, NORMAL,        PHYSICAL,  90, 15,  10
 	move FACADE,       EFFECT_FACADE,             70, NORMAL,        PHYSICAL, 100, 24,   0
 	move HEX,          EFFECT_HEX,                65, GHOST,         SPECIAL,  100, 16,   0
