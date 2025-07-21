05419119d9f169f733e3f6d51db93f0de7791790
RaJah239 <ravijagroo239@gmail.com>
2025-07-21 10:52:52 -0400

    Mirror Shot added

--- data/moves/descriptions.asm
+++ data/moves/descriptions.asm
@@ -148,7 +148,7 @@ MoveDescriptions::
 	dw CalmMindDescription
 	dw ShadowSneakDescription
 	dw SporeDescription
-	dw FlashDescription
+	dw MirrorShotDescription
 	dw PsywaveDescription
 	dw SplashDescription
 	dw AcidArmorDescription
@@ -771,8 +771,8 @@ SporeDescription:
 	db   "A move that"
 	line "induces sleep.@"
 
-FlashDescription:
-	db   "Blinds the foe to"
+MirrorShotDescription:
+	db   "An attack thay may"
 	line "reduce accuracy.@"
 
 PsywaveDescription:
