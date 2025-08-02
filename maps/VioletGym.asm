928f9d8b329214edf76155bf060d6e4f37455c45
RaJah239 <ravijagroo239@gmail.com>
2025-08-02 08:01:38 -0400

    Violet City's first warp is now outside the Pokémon Center - needed for Fast Travel

--- maps/VioletGym.asm
+++ maps/VioletGym.asm
@@ -318,8 +318,8 @@ VioletGymGuideWinText:
 
 VioletGym_MapEvents:
 	def_warp_events
-	warp_event  4, 15, VIOLET_CITY, 2
-	warp_event  5, 15, VIOLET_CITY, 2
+	warp_event  4, 15, VIOLET_CITY, 3
+	warp_event  5, 15, VIOLET_CITY, 3
 
 	def_coord_events
 
