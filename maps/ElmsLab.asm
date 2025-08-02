6b5b494c4393dc624a0b3a8ecf106bcd43166fbf
RaJah239 <ravijagroo239@gmail.com>
2025-08-02 05:58:16 -0400

    New Bark Town's first warp is outside Player's House - needed for Fast Travel

--- maps/ElmsLab.asm
+++ maps/ElmsLab.asm
@@ -1374,8 +1374,8 @@ ElmsLabPorygonPCScript:
 
 ElmsLab_MapEvents:
 	def_warp_events
-	warp_event  4, 11, NEW_BARK_TOWN, 1
-	warp_event  5, 11, NEW_BARK_TOWN, 1
+	warp_event  4, 11, NEW_BARK_TOWN, 2
+	warp_event  5, 11, NEW_BARK_TOWN, 2
 
 	def_coord_events
 	coord_event  4,  6, SCENE_ELMSLAB_CANT_LEAVE, LabTryToLeaveScript
