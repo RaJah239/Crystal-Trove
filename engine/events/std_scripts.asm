1a638ffea91623df8850c98cdc853e14b22ec2dd
RaJah239 <ravijagroo239@gmail.com>
2025-08-05 13:25:39 -0400

    Revert "Minimal dialogue: PokeCentre Chansey quick heals"
    This reverts commit 1ccfa80730d3a8340689c8861d3584f2c189d30d.

--- engine/events/std_scripts.asm
+++ engine/events/std_scripts.asm
@@ -78,26 +78,6 @@ SkipIntroScript:
 	end
 
 ChanseyPokeCenterScript:
-	isdialogueminimal
-	iftrue .quickhealinstead
-	scall ChanseyCryPokePicScript
-	sjump .done
-.quickhealinstead:
-	scall ChanseyHealsOWScript
-	turnobject PLAYER, DOWN
-.done:
-	end
-
-ChanseyHealsOWScript:
-	scall ChanseyCryPokePicScript
-	special FadeOutPalettes
-	special LoadMapPalettes
-	playsound SFX_FULL_HEAL
-	special HealParty
-	special FadeInPalettes_EnableDynNoApply
-	end
-
-ChanseyCryPokePicScript:
 	faceplayer
 	reanchormap
 	pokepic CHANSEY
@@ -109,6 +89,15 @@ ChanseyCryPokePicScript:
 	farwritetext ChanseyCryText
 	pause 20
 	closetext
+	end
+
+ChanseyHealsOWScript:
+	scall ChanseyPokeCenterScript
+	special FadeOutPalettes
+	special LoadMapPalettes
+	playsound SFX_FULL_HEAL
+	special HealParty
+	special FadeInPalettes_EnableDynNoApply
 	end
 
 PokecenterNurseScript:
