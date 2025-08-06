710b7ecf672c527985aa95a00282140ea0cbf378
RaJah239 <ravijagroo239@gmail.com>
2025-08-06 13:08:49 -0400

    Field Actions: Skipped Bicycle text when used from bag

--- engine/events/overworld.asm
+++ engine/events/overworld.asm
@@ -1970,8 +1970,11 @@ Script_GetOnBike:
 	refreshmap
 	special UpdateTimePals
 	loadvar VAR_MOVEMENT, PLAYER_BIKE
+	checkevent EVENT_QUICK_FIELD_ACTION
+	iftrue .skip
 	writetext GotOnBikeText
 	waitbutton
+.skip
 	closetext
 	special UpdatePlayerSprite
 	end
@@ -1986,8 +1989,12 @@ Script_GetOffBike:
 	refreshmap
 	special UpdateTimePals
 	loadvar VAR_MOVEMENT, PLAYER_NORMAL
+	checkevent EVENT_QUICK_FIELD_ACTION
+	iftrue .skip
 	writetext GotOffBikeText
 	waitbutton
+.skip
+	; fallthrough
 
 FinishGettingOffBike:
 	closetext
