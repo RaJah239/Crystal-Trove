c019b37629ff04aa6c89e56e3aba422380b2a2c3
RaJah239 <ravijagroo239@gmail.com>
2025-08-06 21:15:37 -0400

    Revert "Don't play bike music in National Park"

--- engine/events/overworld.asm
+++ engine/events/overworld.asm
@@ -1903,16 +1903,6 @@ BikeFunction:
 	ld de, Script_GetOnBike_Register
 	call .CheckIfRegistered
 	call QueueScript
-; Don't play bike music in National Park
-	push hl
-	push bc
-	ld de, MAP_MUSIC
-	call GetMapField
-	ld a, c
-	pop bc
-	pop hl
-	cp MUSIC_NATIONAL_PARK
-	jr z, .skip_music
 	xor a
 	ld [wMusicFade], a
 	ld de, MUSIC_NONE
@@ -1923,7 +1913,6 @@ BikeFunction:
 	ld a, e
 	ld [wMapMusic], a
 	call PlayMusic
-.skip_music
 	ld a, $1
 	ret
 
