545952b8c5f8aefa2da311fb17a9db1acd4a0420
RaJah239 <ravijagroo239@gmail.com>
2025-09-03 10:02:44 -0400

    Tail call optimisation home/string.asm

--- home/string.asm
+++ home/string.asm
@@ -7,6 +7,8 @@ InitName::
 ; Intended for names, so this function is limited to ten characters.
 	push hl
 	ld c, NAME_LENGTH - 1
+	; fallthrough
+
 _InitString::
 ; if the string pointed to by hl is empty (defined as "zero or more spaces
 ; followed by a null"), then initialize it to the string pointed to by de.
@@ -26,8 +28,7 @@ _InitString::
 	pop de
 	ld b, 0
 	inc c
-	call CopyBytes
-	ret
+	jmp CopyBytes
 
 .notblank
 	pop bc
