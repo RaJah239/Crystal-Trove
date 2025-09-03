dcf4e3b5d56d0ca63de6e04628e7e6096f06f9b6
RaJah239 <ravijagroo239@gmail.com>
2025-09-02 20:29:47 -0400

    Deleted unreferenced code in mobile/fixed_words.asm

--- mobile/fixed_words.asm
+++ mobile/fixed_words.asm
@@ -31,15 +31,6 @@ Function11c075:
 	call Function11c08f
 	ret
 
-Function11c082: ; unreferenced
-	push de
-	ld a, c
-	call Function11c254
-	pop de
-	ld bc, wcd36
-	call PrintEZChatBattleMessage
-	ret
-
 Function11c08f:
 	ld l, e
 	ld h, d
@@ -1334,36 +1325,11 @@ Function11c86e:
 	jr nz, .asm_11c8c2
 	ret
 
-BCD2String: ; unreferenced
-	inc a
-	push af
-	and $f
-	ldh [hDividend], a
-	pop af
-	and $f0
-	swap a
-	ldh [hDividend + 1], a
-	xor a
-	ldh [hDividend + 2], a
-	push hl
-	farcall Function11a80c
-	pop hl
-	ld a, [wcd63]
-	add "０"
-	ld [hli], a
-	ld a, [wcd62]
-	add "０"
-	ld [hli], a
-	ret
-
-MobileString_Page: ; unreferenced
-	db "ぺージ@"
-
 MobileString_Prev:
-	db "まえ@"
+	db "@"
 
 MobileString_Next:
-	db "つぎ@"
+	db "@"
 
 Function11c8f6:
 	ld a, [wcd20]
