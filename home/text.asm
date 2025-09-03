a79c93eab5b597e168faf1e2a4e298eba83ec30a
RaJah239 <ravijagroo239@gmail.com>
2025-09-03 18:00:04 -0400

    Updated home/text.asm

--- home/text.asm
+++ home/text.asm
@@ -136,8 +136,7 @@ CreateBoxBorders::
 SpeechTextbox::
 ; Standard textbox.
 	hlcoord TEXTBOX_X, TEXTBOX_Y
-	ld b, TEXTBOX_INNERH
-	ld c, TEXTBOX_INNERW
+	lb bc, TEXTBOX_INNERH, TEXTBOX_INNERW
 	jr Textbox
 
 RadioTerminator::
