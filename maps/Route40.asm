9670adfa965616ddfe7aa814e8cb4289b913912c
RaJah239 <ravijagroo239@gmail.com>
2025-09-18 16:06:38 -0400

    Updated Driftsea

--- maps/Route40.asm
+++ maps/Route40.asm
@@ -6,7 +6,7 @@ Route40_MapEvents:
 	def_coord_events
 
 	def_bg_events
-	bg_event 13,  5, BGEVENT_READ, SeaSign
+	bg_event 12,  5, BGEVENT_READ, SeaSign
 
 	def_object_events
 	object_event  6, 10, SPRITE_ROCK, SPRITEMOVEDATA_SMASHABLE_ROCK, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route40Rock, -1
@@ -32,6 +32,8 @@ SeaSign:
 	jumptext SeaSignText
 
 SeaSignText:
-	text "Catch sea dwellers"
+	text "Driftsea"
+
+	para "Catch sea dwellers"
 	line "in every ripple!"
 	done
