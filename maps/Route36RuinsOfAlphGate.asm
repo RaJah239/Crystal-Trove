5855a42b0ddfef67679b5017d594fc38b2366c82
RaJah239 <ravijagroo239@gmail.com>
2025-09-18 10:59:55 -0400

    Ruins of Alph connection added

--- maps/Route36RuinsOfAlphGate.asm
+++ maps/Route36RuinsOfAlphGate.asm
@@ -1,6 +1,21 @@
+Route36RuinsOfAlphGate_MapEvents:
+	def_warp_events
+	warp_event  4,  0, BATTLE_TOWER_OUTSIDE, 4
+	warp_event  5,  0, BATTLE_TOWER_OUTSIDE, 3
+	warp_event  4,  7, RUINS_OF_ALPH_OUTSIDE, 9
+	warp_event  5,  7, RUINS_OF_ALPH_OUTSIDE, 9
+
+	def_coord_events
+
+	def_bg_events
+
 	object_const_def
 	const ROUTE36RUINSOFALPHGATE_OFFICER
 	const ROUTE36RUINSOFALPHGATE_GRAMPS
+
+	def_object_events
+	object_event  0,  4, SPRITE_OFFICER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, Route36RuinsOfAlphGateOfficerScript, -1
+	object_event  7,  5, SPRITE_GRAMPS, SPRITEMOVEDATA_WANDER, 1, 2, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, Route36RuinsOfAlphGateGrampsScript, -1
 
 Route36RuinsOfAlphGate_MapScripts:
 	def_scene_scripts
@@ -31,18 +46,3 @@ Route36RuinsOfAlphGateGrampsText:
 	para "are visiting the"
 	line "RUINS OF ALPH."
 	done
-
-Route36RuinsOfAlphGate_MapEvents:
-	def_warp_events
-	warp_event  4,  0, ROUTE_36, 3
-	warp_event  5,  0, ROUTE_36, 4
-	warp_event  4,  7, RUINS_OF_ALPH_OUTSIDE, 9
-	warp_event  5,  7, RUINS_OF_ALPH_OUTSIDE, 9
-
-	def_coord_events
-
-	def_bg_events
-
-	def_object_events
-	object_event  0,  4, SPRITE_OFFICER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, Route36RuinsOfAlphGateOfficerScript, -1
-	object_event  7,  5, SPRITE_GRAMPS, SPRITEMOVEDATA_WANDER, 1, 2, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, Route36RuinsOfAlphGateGrampsScript, -1
