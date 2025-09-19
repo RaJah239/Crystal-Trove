f5cdca2f36148fab529dd8aa7152292b35686e5a
RaJah239 <ravijagroo239@gmail.com>
2025-09-19 15:20:53 -0400

    Updated maps/BattleTowerOutside.asm

--- maps/BattleTowerOutside.asm
+++ maps/BattleTowerOutside.asm
@@ -26,16 +26,8 @@ BattleTowerOutside_MapEvents:
 	bg_event 22, 21, BGEVENT_READ, PokeBrosSign
 
 	object_const_def
-	const BATTLETOWEROUTSIDE_STANDING_YOUNGSTER
-	const BATTLETOWEROUTSIDE_BEAUTY
-	const BATTLETOWEROUTSIDE_SAILOR
-	const BATTLETOWEROUTSIDE_LASS
 
 	def_object_events
-	object_event 45, 10, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, BattleTowerOutsideYoungsterScript, -1
-	object_event 56, 11, SPRITE_BEAUTY, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, BattleTowerOutsideBeautyScript, -1
-	object_event 14, 10, SPRITE_SAILOR, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, BattleTowerOutsideSailorScript, -1
-	object_event 22, 11, SPRITE_LASS, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
 
 BattleTowerOutside_MapScripts:
 	def_scene_scripts
@@ -93,51 +85,11 @@ PokeBrosSignText:
 	line "and your friends!"
 	done
 
-BattleTowerOutsideYoungsterScript:
-	jumptextfaceplayer BattleTowerOutsideYoungsterText
-
-BattleTowerOutsideYoungsterText:
-	text "Wow, the BATTLE"
-	line "TOWER is huge!"
-
-	para "There must be many"
-	line "kinds of #MON"
-	cont "in there!"
-	done
-
-BattleTowerOutsideBeautyScript:
-	jumptextfaceplayer BattleTowerOutsideBeautyText
-
-BattleTowerOutsideBeautyText:
-	text "You can use only"
-	line "three #MON."
-
-	para "It's so hard to"
-	line "decide which three"
-
-	para "should go into"
-	line "battle…"
-	done
-
-BattleTowerOutsideSailorScript:
-	jumptextfaceplayer BattleTowerOutsideSailorText
-
-BattleTowerOutsideSailorText:
-	text "Hehehe, I snuck"
-	line "out from work."
-
-	para "I can't bail out"
-	line "until I've won!"
-
-	para "I have to win it"
-	line "all. That I must!"
-	done
-
 BattleTowerOutsideSign:
 	jumptext BattleTowerOutsideSignText
 
 BattleTowerOutsideSignText:
-	text "BATTLE TOWER"
+	text "Battle Tower"
 
 	para "Take the Ultimate"
 	line "Trainer Challenge!"
