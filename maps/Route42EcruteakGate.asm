1864e81acc933385c53353c7931c34e5373f432b
RaJah239 <ravijagroo239@gmail.com>
2025-09-19 16:07:25 -0400

    Updated maps/Route42EcruteakGate.asm

--- maps/Route42EcruteakGate.asm
+++ maps/Route42EcruteakGate.asm
@@ -9,12 +9,11 @@ Route42EcruteakGate_MapEvents:
 
 	def_bg_events
 
-	object_const_def
-	const ROUTE42ECRUTEAKGATE_OFFICER
-	const ROUTE42ECRUTEAKGATE_OFFICER_BLOCKER
-
 	def_object_events
 	object_event  5,  2, SPRITE_OFFICER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, Route42EcruteakGateOfficerScript, -1
+
+	object_const_def
+	const ROUTE42ECRUTEAKGATE_OFFICER
 
 Route42EcruteakGate_MapScripts:
 	def_scene_scripts
@@ -22,12 +21,8 @@ Route42EcruteakGate_MapScripts:
 	def_callbacks
 
 Route42EcruteakGateOfficerScript:
-	jumptextfaceplayer Route42EcruteakGateOfficerText
+	jumptext Route42EcruteakGateOfficerText
 
 Route42EcruteakGateOfficerText:
-	text "MT.MORTAR is like"
-	line "a maze inside."
-
-	para "Be careful. Don't"
-	line "get lost in there."
+	text "Go on through."
 	done
